cd369e09a82bf6ff7dd2ae451d1ef7875a9cbe43
Timosssshka <112942954+Timosssshka@users.noreply.github.com>
2022-11-18 17:18:31 +0300

    Update form.js

--- form.js
+++ form.js
@@ -1,5 +1,5 @@
 let openPopupButtons = document.querySelectorAll('.bttn1');
-let form = document.querySelector('.forma');
+let form = document.querySelector('#forma');
 let popup = document.querySelector('.popup');
 
 let names = document.getElementById('name');
@@ -12,43 +12,41 @@ function save() {
   localStorage.setItem('Почта', email.value);
   localStorage.setItem('Сообщение', massage.value);
   if (checkbx.checked) {
-    localStorage.setItem('Согласие', 1);
+    localStorage.setItem('Чекбокс', 1);
   } else {
-    localStorage.setItem('Согласие', 0);
+    localStorage.setItem('Чекбокс', 0);
   }
 }
-
 document.addEventListener('DOMContentLoaded', () => {
   names.value = localStorage.getItem('Имя');
-  email.value = localStorage.getItem('Эл. почта');
+  email.value = localStorage.getItem('Почта');
   massage.value = localStorage.getItem('Сообщение');
-  let checkBox = localStorage.getItem('Согласие');
+  let checkBox = localStorage.getItem('Чекбокс');
   if (checkBox == 1) {
     checkbx.checked = true;
   } else if (checkBox == 0) {
     checkbx.checked = false;
   }
-  
+
   names.oninput = save;
   email.oninput = save;
   massage.oninput = save;
   checkbx.oninput = save;
-
+  
   openPopupButtons.forEach((button) => {
-  button.addEventListener('click', (event) => {
-    event.preventDefault();
-    window.onpopstate = function () {
-      window.history.go(0);
-      form.classList.remove('open');
-      popup.classList.remove('popup_open');
-    };
-    history.pushState({ page: 1 }, '', '?forma');
-    form.classList.add('open');
-    popup.classList.add('popup_open');
+    button.addEventListener('click', (event) => {
+      event.preventDefault();
+      window.onpopstate = function () {
+        window.history.go(0);
+        form.classList.remove('open');
+        popup.classList.remove('popup_open');
+      };
+      history.pushState({ page: 1 }, '', '?forma');
+      form.classList.add('open');
+      popup.classList.add('popup_open');
+    });
   });
-});
-
-
+  
     $(function(){
       $(".forms").submit(function(e){
           e.preventDefault();
